e1faa109be93551ac99411bf00cb64c93721831b
André Marques <andregm_7@hotmail.com>
2019-03-13 10:58:21 +0000

    INT: Vue icons

--- sources/components/transitionComponent.vue
+++ sources/components/transitionComponent.vue
@@ -13,25 +13,61 @@
     height: 0;
     position: fixed;
     top: 0;
+    overflow: hidden;
     z-index: 1;
+
+    &:before {
+        content: '';
+        width: 50px;
+        height: 50px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background: $white;
+        border: 1px solid red;
+        border-radius: 50%;
+        z-index: 1;
+    }
 
     .leftBar, .rightBar {
         width: 50%;
         height: 100%;
         position: absolute;
         top: 0;
+
+        &:before {
+            content: '';
+            width: 99%;
+            height: 2px;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            background: $orange;
+        }
+
     }
 
     .leftBar {
         left: 0;
         background: $primaryGradient;
         transform: translateX(-100%);
+
+        &:before {
+            left: 0;
+        }
+
     }
 
     .rightBar {
         right: 0;
         background: $invertedPrimaryGradient;
         transform: translateX(100%);
+
+        &:before {
+            right: 0;
+        }
+
     }
 
 }
