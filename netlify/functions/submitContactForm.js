25f2a67700ad0b21a6f39f5de142bbbbd1963ecb
André Marques <andre.marques@farfetch.com>
2023-04-12 23:27:36 +0100

    feat: Change nodemailer host from Outlook to Zoho Mail

--- netlify/functions/submitContactForm.js
+++ netlify/functions/submitContactForm.js
@@ -4,21 +4,18 @@ exports.handler = async function (event, context) {
     const data = JSON.parse(event.body);
 
     const smtpTransport = nodemailer.createTransport({
-        host: 'smtp-mail.outlook.com',
-        port: 587,
-        secure: false,
-        tls: {
-            ciphers: 'SSLv3'
-        },
+        host: 'smtp.zoho.eu',
+        port: 465,
+        secure: true,
         auth: {
-            user: process.env.NODEMAILER_SEND_EMAIL_FROM_AND_TO,
-            pass: process.env.NODEMAILER_SEND_EMAIL_FROM_AND_TO_PASS,
+            user: process.env.NODEMAILER_SEND_EMAIL_FROM,
+            pass: process.env.NODEMAILER_SEND_EMAIL_FROM_PASS,
         },
     });
 
     const mailOptions = {
-        from: process.env.NODEMAILER_SEND_EMAIL_FROM_AND_TO,
-        to: process.env.NODEMAILER_SEND_EMAIL_FROM_AND_TO,
+        from: process.env.NODEMAILER_SEND_EMAIL_FROM,
+        to: process.env.NODEMAILER_SEND_EMAIL_TO,
         subject: `AndreMarquesDev.com - Contacto de ${data.name}${data.company && ' da empresa ' + data.company}`,
         html: `<p>Email: ${data.email}</p><br/><p>${data.message}</p>`,
     };
